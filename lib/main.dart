b6a09de42c52956bf17a448c7aa8011639c99bb7
rafaleal <rafaelalealmartinss@gmail.com>
2020-09-28 23:38:46 -0300

    add save data function and read data function to _HomeState

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:html';
 
 import 'package:flutter/material.dart';
@@ -14,12 +15,33 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _toDoList = [];
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
-}
-Future<File> _getFile() async {
-  final directory = await getApplicationDocumentsDirectory();
-  return File("${directory.path}/data.json");
+
+  Future<File> _getFile() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return File("${directory.path}/data.json");
+  }
+
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+
+    final file = await _getFile();
+    return file.writeAsString(data);
+  }
+
+  Future<String> _readData() async {
+    try {
+      final file = await _getFile();
+
+      return file.readAsString();
+    } catch(e) {
+      return e;
+    }
+  }
+
 }
